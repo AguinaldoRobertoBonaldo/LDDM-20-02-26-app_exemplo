e588241914009a10223cad8b8fe077e35686a486
AguinaldoRobertoBonaldo <aguinaldoroberto@gmail.com>
2026-02-21 01:00:19 +0000

    aula2_expo

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Olá, Mundo!!!!</Text>
+      <Button title="Clique aqui"
+      onPress={() => alert('Botão Clicado!')}
+
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 30,
+    color: 'red',
+  }
+});
